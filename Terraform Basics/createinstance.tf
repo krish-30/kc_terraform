b5bc9ab3159328f9ae3d79b3e8e2b8fbc792a30e
KC <krishnachaitanya.30@gmail.com>
2021-05-29 20:45:30 +0530

    Updated typos

--- Terraform Basics/createinstance.tf
+++ Terraform Basics/createinstance.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    access_key = AKIA3DBQ3EYKWX72HR7L
-    secret_key = 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
+    access_key = "AKIA3DBQ3EYKWX72HR7L"
+    secret_key = "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"
+    region     = "us-east-1"
 }
+
 resource "aws_instance" "Myfirstinstance" {
-    ami = 	ami-0d563aeddd4be7fff
-    instance_type = t2.micro
+    ami           = "ami-0d563aeddd4be7fff"
+    instance_type = "t2.micro"
 }
