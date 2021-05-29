c7078d86ccb897be64431279e082e61c74bb5730
KC <krishnachaitanya.30@gmail.com>
2021-05-29 20:53:31 +0530

    region udpated

--- Terraform Basics/createinstance.tf
+++ Terraform Basics/createinstance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     access_key = "AKIA3DBQ3EYKWX72HR7L"
     secret_key = "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"
-    region     = "us-east-1"
+    region     = "us-east-2"
 }
 
 resource "aws_instance" "Myfirstinstance" {
