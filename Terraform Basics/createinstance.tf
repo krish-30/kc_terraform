3e459fca4a1675b49aa798ccfe22520cc0ff43df
KC <krishnachaitanya.30@gmail.com>
2021-05-29 20:37:18 +0530

    "Updated instance"

---
+++ Terraform Basics/createinstance.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    access_key = AKIA3DBQ3EYKWX72HR7L
+    secret_key = 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
+}
+resource "aws_instance" "Myfirstinstance" {
+    ami = 	ami-0d563aeddd4be7fff
+    instance_type = t2.micro
+}
